d2ea7a47a4ef4848c840d3ee532f9b9fb77f8faa
arnaudbagnis <arnaud.bagnis@gmail.com>
2026-03-01 19:18:45 +0100

    Initial Commit

---
+++ LocalChat/LocalChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalChatApp.swift
+//  LocalChat
+//
+//  Created by Arnaud Bagnis on 01/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct LocalChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
